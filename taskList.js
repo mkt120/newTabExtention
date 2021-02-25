cb3e41a318b97dfdcc340e2e93fa0166201f9193
mkt120 <dev.mkt120.elop@gmail.com>
2021-02-25 23:00:11 +0900

    edit taskList

--- taskList.js
+++ taskList.js
@@ -34,10 +34,12 @@ function createTaskList() {
 function createHeader(document) {
     var header = document.createElement('div');
     header.classList.add(CLASS_TASK_LIST_HEADER);
-    header.textContent = "タスクリスト";
+    var span = document.createElement('span');
+    span.textContent = "タスクリスト";
+    header.appendChild(span);
 
     // 追加するボタン
-    var addButton = document.createElement('button');
+    var addButton = document.createElement('div');
     addButton.textContent = "追加";
     addButton.classList.add(CLASS_TASK_ITEM_ADD_BUTTON_VIEW);
     addButton.onclick = function(event) {
@@ -74,17 +76,21 @@ function createList() {
 
 function createTaskItemView(taskInfo) {
     // 親View
-    var p = document.createElement('p');
-    p.classList.add(CLASS_TASK_ITEM_VIEW);
-    let title = taskInfo[TASK_INFO_TITLE];
+    var div = document.createElement('div');
+    div.classList.add(CLASS_TASK_ITEM_VIEW);
 
     // 要素の設定
+    var p = document.createElement('span');
+    p.classList.add(CLASS_TASK_ITEM_VIEW);
+    let title = taskInfo[TASK_INFO_TITLE];
     if (title != null && title.length > 0) {
         // タイトル・URLを設定
         p.textContent = title;
+        div.appendChild(p);
     }
+
     // 完了ボタン
-    let button = document.createElement('button');
+    let button = document.createElement('div');
     button.classList.add(CLASS_TASK_ITEM_COMPLETE_BUTTON_VIEW);
 
     button.textContent = '完了';
@@ -92,8 +98,8 @@ function createTaskItemView(taskInfo) {
         let list = removeTaskInfo(taskInfo);
         createList(list);
     };
-    p.appendChild(button);
-    return p;
+    div.appendChild(button);
+    return div;
 }
 
 function saveTaskInfo(taskInfo) {
