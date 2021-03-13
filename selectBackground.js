e355473ff84f3f8bed55c39eadf7469b73b4bdca
mkt120 <dev.mkt120.elop@gmail.com>
2021-03-13 19:34:02 +0900

    edit selectBackground

--- selectBackground.js
+++ selectBackground.js
@@ -1,11 +1,26 @@
+const BACKGROUND_IMAGE = "background_image";
+window.addEventListener('load', function () {
+    console.log("setBackground document.cookie=" + document.cookie);
+    chrome.storage.local.get(BACKGROUND_IMAGE, function (value) {
+        var result = value[BACKGROUND_IMAGE];
+        setBackground(result);
+    });
+});
+
 function onSelectFile(e) {
     var reader = new FileReader();
-    reader.onload = function(e) {
-        var body = document.body;
-        body.style.background = 'url(' + e.target.result + ')';
-        body.style.backgroundSize = 'cover';
-        body.style.backgroundRepeat = 'no-repeat';
-        body.style.backgroundPosition = 'center center';
+    reader.onload = function (e) {
+        chrome.storage.local.set({ 'background_image': e.target.result }, function () {});
+        setBackground(e.target.result);
     }
     reader.readAsDataURL(e.target.files[0]);
+}
+
+function setBackground(result) {
+    console.log("setBackground result=" + result);
+    var body = document.body;
+    body.style.background = 'url(' + result + ')';
+    body.style.backgroundSize = 'cover';
+    body.style.backgroundRepeat = 'no-repeat';
+    body.style.backgroundPosition = 'center center';
 }
